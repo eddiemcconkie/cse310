c4ad40c3142de693097554589ee0636664b6053f
JacobHornbeck <hor20009@byui.edu>
2021-05-17 20:03:25 -0600

    Fix editing

--- desktop-app/controller.js
+++ desktop-app/controller.js
@@ -1,8 +1,9 @@
 const fs = require("fs")
 const path = require("path")
+const classDataFilePath = path.join(__dirname,'data','classes.json')
 
 function readClassFile(cb) {
-    fs.readFile(path.join(__dirname,'data','classes.json'), (err, data) => {
+    fs.readFile(classDataFilePath, (err, data) => {
         if (err) return console.log(err)
         cb(JSON.parse(data))
     })
@@ -17,7 +18,39 @@ exports.getHomepage = (req, res, next) => {
 }
 
 exports.getEditClass = (req, res, next) => {
+    readClassFile(classData => {
+        const editClass = classData.filter(singleClass => {
+            return singleClass.id === req.body.editClassId
+        })
+        res.render('pages/class-details-form.ejs', {
+            editing: true,
+            classData: editClass[0]
+        })
+    })
+}
 
+exports.postEditClass = (req, res, next) => {
+    readClassFile(classData => {
+        const days = [req.body.Monday,req.body.Tuesday,req.body.Wednesday,req.body.Thursday,req.body.Friday]
+        let allClasses = [...classData]
+        const classIndex = classData.map((singleClass, index) => {
+            return {prevIndex: index, id: singleClass.id}
+        }).filter(elements => {
+            return elements.id === req.body.editedClassId
+        })[0].prevIndex
+
+        allClasses[classIndex].name = req.body.className
+        allClasses[classIndex].days = days.filter(day => day)
+        allClasses[classIndex].times = [req.body.classStartTime, req.body.classEndTime]
+        allClasses[classIndex].semester = req.body.classSemester
+        allClasses[classIndex].section = req.body.classSection
+
+
+        fs.writeFile(classDataFilePath, JSON.stringify(allClasses), (err) => {
+            if (err) console.log(err)
+            else res.redirect('/')
+        })
+    })
 }
 
 exports.getAddClass = (req, res, next) => {
@@ -28,26 +61,21 @@ exports.getAddClass = (req, res, next) => {
 
 exports.postAddClass = (req, res, next) => {
     readClassFile(data => {
+        const days = [req.body.Monday,req.body.Tuesday,req.body.Wednesday,req.body.Thursday,req.body.Friday]
         const courseData = {
             name: req.body.className,
-            times: {
-                days: [
-                    req.body.Monday,
-                    req.body.Tuesday,
-                    req.body.Wednesday,
-                    req.body.Thursday,
-                    req.body.Friday
-                ],
-                times: [req.body.classStartTime, req.body.classEndTime]
-            },
-            id: req.body.className.toLowerCase().replace(/\ /g, '')+req.body.classSemester+(new Date().getFullYear().toString())+"s"+req.body.classSection
+            days: days.filter(day => day),
+            times: [req.body.classStartTime, req.body.classEndTime],
+            semester: req.body.classSemester,
+            section: req.body.classSection,
+            id: req.body.className.toLowerCase().replace(/\ /g, '')+req.body.classSemester.toLowerCase()+(new Date().getFullYear().toString())+"s"+req.body.classSection
         }
 
         const courses = [...data]
         courses.push(courseData)
 
-        fs.writeFile(path.join(__dirname,'data','classes.json'), JSON.stringify(courses), (err) => {
-            if (err) console.log("ERROR:", err)
+        fs.writeFile(classDataFilePath, JSON.stringify(courses), (err) => {
+            if (err) console.log(err)
             else res.redirect('/')
         })
     })
--- desktop-app/routes.js
+++ desktop-app/routes.js
@@ -5,6 +5,8 @@ const router = express.Router()
 router.get('/class/:classId/qr-code', controller.getQR)
       .get('/add-class', controller.getAddClass)
       .post('/add-class', controller.postAddClass)
+      .post('/edit-class-view', controller.getEditClass)
+      .post('/edit-class', controller.postEditClass)
       .get('/', controller.getHomepage)
 
 module.exports = router
