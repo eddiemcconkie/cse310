082dc51e00b832f1ff26026891d87fd184f36a0d
Edward <edwardmcconkie@gmail.com>
2021-06-16 16:06:53 -0600

    Fixed video size

--- desktop-app/server/video.js
+++ desktop-app/server/video.js
@@ -1,4 +1,4 @@
 function renderVideo() { 
-    document.querySelector("#page").innerHTML = '<iframe width="1904" height="768" src="https://www.youtube.com/embed/ApfLlubBZ7A?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+    document.querySelector("#page").innerHTML = '<iframe src="https://www.youtube.com/embed/ApfLlubBZ7A?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 
 }
