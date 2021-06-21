940d00fefe9cbcde22e01f1960cb9feba40b0652
JacobHornbeck <hor20009@byui.edu>
2021-06-21 15:57:04 -0600

    Mobile app

--- mobile-app/app/src/androidTest/java/com/cse310/group_project/ExampleInstrumentedTest.kt
+++ mobile-app/app/src/androidTest/java/com/cse310/group_project/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.mabileapp
+package com.cse310.group_project
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.mabileapp", appContext.packageName)
+        assertEquals("com.cse310.group_project", appContext.packageName)
     }
 }
--- mobile-app/app/src/main/java/com/cse310/group_project/MainActivity.kt
+++ mobile-app/app/src/main/java/com/cse310/group_project/MainActivity.kt
@@ -1,22 +1,31 @@
-package com.example.mabileapp
+package com.cse310.group_project
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
-import com.example.mabileapp.databinding.ActivityMainBinding
+import com.cse310.group_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
 
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -33,5 +42,11 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
